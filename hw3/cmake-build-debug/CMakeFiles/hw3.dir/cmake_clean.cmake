c15ba62218b03d569f71042901d823fdfbb14491
shirredner <shirredner@gmail.com>
2018-05-12 11:39:35 +0300

    first commit

---
+++ hw3/cmake-build-debug/CMakeFiles/hw3.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hw3.dir/main.c.obj"
+  "CMakeFiles/hw3.dir/map_mtm.c.obj"
+  "hw3.pdb"
+  "hw3.exe"
+  "hw3.exe.manifest"
+  "libhw3.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/hw3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
